36b391805ea332ca631ab11133c3eecbfb95c23a
anapgh <ap.garcia.2016@alumnos.urjc.es>
2020-03-12 10:03:58 +0100

    Dibujado raqueta

---
+++ P3/L7/PintandoElementos2.js
@@ -0,0 +1,10 @@
+//-- Inicializamos en JS
+console.log('Empezando el JS...');
+
+function main(){
+  console.log('Pong: Main: Start!');
+
+  var canvas = document.getElementById('canvas');
+  canvas.width = 600;
+  canvas.height = 400;
+}
