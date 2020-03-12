610bbd1959ee8139f52e0837d251626a27521191
anapgh <ap.garcia.2016@alumnos.urjc.es>
2020-03-12 10:07:48 +0100

    dibujada segunda raqueta

--- P3/L7/PintandoElementos2.js
+++ P3/L7/PintandoElementos2.js
@@ -4,7 +4,19 @@ console.log('Empezando el JS...');
 function main(){
   console.log('Pong: Main: Start!');
 
+  //-- Obtener el objeto canvas y asignar sus dimensiones
   var canvas = document.getElementById('canvas');
   canvas.width = 600;
   canvas.height = 400;
+
+  //-- Sacamos el contexto para poder dibujar en el canvas
+  var ctx = canvas.getContext('2d');
+
+  //--Dibujo raqueta1
+  ctx.fillStyle = 'white'; // color relleno
+  ctx.fillRect(50,100,10,40); //rellenar recta - x,y,anchura, Altura
+
+  //--Dibujo raqueta2
+  ctx.fillStyle = 'white';
+  ctx.fillRect(550,300,10,40);
 }
