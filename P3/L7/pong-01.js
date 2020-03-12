ea8e8e9b407f1277387d3a723fe3c4576a1df9ab
anapgh <ap.garcia.2016@alumnos.urjc.es>
2020-03-12 09:45:52 +0100

    Nos dibuja la bola

--- P3/L7/pong-01.js
+++ P3/L7/pong-01.js
@@ -8,4 +8,12 @@ const canvas = document.getElementById('canvas');
 console.log(`canvas: Anchura: ${canvas.width}, Altura: ${canvas.height}`);
 
 //-- Para poder dibujar en el canvas hay que obtener su contexto
-const ctx = canvas.getContext('2D');
+const ctx = canvas.getContext('2d');
+
+//-- Dibujar bola
+ctx.beginPath(); // metodo que se llama para el estilo de cada elemento
+ctx.fillStyle = 'white';
+
+//-- x,y,anchura, Altura
+ctx.rect(100,200,10,10);
+ctx.fill();
