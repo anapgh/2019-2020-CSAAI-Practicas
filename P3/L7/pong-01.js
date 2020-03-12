301f506e14dc6c9b402181865f4084cac51838d3
anapgh <ap.garcia.2016@alumnos.urjc.es>
2020-03-12 09:39:08 +0100

    obteniendo el contexto, para poder pintar en el canvas

--- P3/L7/pong-01.js
+++ P3/L7/pong-01.js
@@ -6,3 +6,6 @@ const canvas = document.getElementById('canvas');
 //-- Sus dimensiones las hemos fijado en el fichero HTML
 //-- Las imprimimos en la consola
 console.log(`canvas: Anchura: ${canvas.width}, Altura: ${canvas.height}`);
+
+//-- Para poder dibujar en el canvas hay que obtener su contexto
+const ctx = canvas.getContext('2D');
