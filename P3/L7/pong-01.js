6e7dc0b87bfaf26c3eb0464ba59a0ac592f215a9
anapgh <ap.garcia.2016@alumnos.urjc.es>
2020-03-12 09:33:25 +0100

    son tildes, no comillas

--- P3/L7/pong-01.js
+++ P3/L7/pong-01.js
@@ -5,4 +5,4 @@ const canvas = document.getElementById('canvas');
 
 //-- Sus dimensiones las hemos fijado en el fichero HTML
 //-- Las imprimimos en la consola
-console.log('canvas-> Anchura: ${canvas.width}, Altura: ${canvas.height}');
+console.log(`canvas: Anchura: ${canvas.width}, Altura: ${canvas.height}`);
