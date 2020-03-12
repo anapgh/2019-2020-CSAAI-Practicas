e24352b4f8059f1b249ba3046a9b30344479b681
anapgh <ap.garcia.2016@alumnos.urjc.es>
2020-03-12 10:20:57 +0100

    Pinta raquetas, bolas, y red

--- P3/L7/PintandoElementos2.js
+++ P3/L7/PintandoElementos2.js
@@ -14,9 +14,40 @@ function main(){
 
   //--Dibujo raqueta1
   ctx.fillStyle = 'white'; // color relleno
-  ctx.fillRect(50,100,10,40); //rellenar recta - x,y,anchura, Altura
+  ctx.fillRect(50,100,10,40); //rellenar rectangulo - x,y,anchura, Altura
 
   //--Dibujo raqueta2
   ctx.fillStyle = 'white';
   ctx.fillRect(550,300,10,40);
+
+  //--Dibujo la bola
+  ctx.fillStyle ='white';
+  ctx.rect(370,200,10,10);
+  ctx.fill();
+
+  //--Dibujo la red
+  console.log('dibujando red');
+  ctx.moveTo(300,1);
+  ctx.lineTo(300,20);
+  ctx.moveTo(300,40);
+  ctx.lineTo(300,60);
+  ctx.moveTo(300,80);
+  ctx.lineTo(300,100);
+  ctx.moveTo(300,120);
+  ctx.lineTo(300,140);
+  ctx.moveTo(300,160);
+  ctx.lineTo(300,180);
+  ctx.moveTo(300,200);
+  ctx.lineTo(300,220);
+  ctx.moveTo(300,240);
+  ctx.lineTo(300,260);
+  ctx.moveTo(300,280);
+  ctx.lineTo(300,300);
+  ctx.moveTo(300,320);
+  ctx.lineTo(300,340);
+  ctx.moveTo(300,360);
+  ctx.lineTo(300,380);
+  ctx.strokeStyle = 'white';
+  ctx.lineWidth = 3;
+  ctx.stroke();
 }
