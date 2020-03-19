5fff19474a8035f099fe333c59a6f614b6c93894
anapgh <ap.garcia.2016@alumnos.urjc.es>
2020-03-19 09:32:25 +0100

    rectificado

--- P3/L7/PintandoElementos2/PintandoElementos2.js
+++ P3/L7/PintandoElementos2/PintandoElementos2.js
@@ -1,29 +1,35 @@
 //-- Inicializamos en JS
 console.log('Empezando el JS...');
 
-function main(){
-  console.log('Pong: Main: Start!');
 
   //-- Obtener el objeto canvas y asignar sus dimensiones
   var canvas = document.getElementById('canvas');
   canvas.width = 600;
   canvas.height = 400;
 
+  // Las imprimimos en la consola
+  console.log(`canvas: Anchura: ${canvas.width}, Altura: ${canvas.height}`);
+
   //-- Sacamos el contexto para poder dibujar en el canvas
   var ctx = canvas.getContext('2d');
 
-  //--Dibujo raquetas
-  ctx.fillStyle = 'white'; // color relleno
-  ctx.fillRect(50,100,10,40); //rellenar rectangulo - x,y,anchura, Altura
-  ctx.fillRect(550,300,10,40);
-
   //--Dibujo la bola
+  ctx.beginPath();
   ctx.fillStyle ='white';
   ctx.rect(370,200,10,10);
   ctx.fill();
 
+  //--Dibujo raquetas
+  ctx.beginPath();
+  ctx.fillStyle = 'red'; // color relleno
+  // Raqueta izquierda
+  ctx.fillRect(50,100,10,40); //rellenar rectangulo - x,y,anchura, Altura
+  // Raqueta derecha
+  ctx.fillRect(550,300,10,40);
+
   //--Dibujo la red
   console.log('dibujando red');
+  ctx.beginPath();
   incr = 0
   while(incr <= canvas.height){
     ctx.moveTo(300,incr);
@@ -34,8 +40,9 @@ function main(){
     ctx.lineWidth = 2;
     ctx.stroke();
   }
-   //-- Dibujar puntuacion
-   ctx.font = '50px Arial'
-   ctx.fillText('0', 220,60);
-   ctx.fillText('2', 350,60);
-}
+
+   //-- Dibujar tanteo
+   ctx.font = '100px Arial'
+   ctx.fillStyle = "white";
+   ctx.fillText('0', 220,80);
+   ctx.fillText('2', 350,80);
