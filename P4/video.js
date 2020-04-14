27ec4459202795f8234c62a5d54e8547f74667c3
anapgh <ap.garcia.2016@alumnos.urjc.es>
2020-04-14 10:59:15 +0200

    Añadido los 3 videos reproduciendose de forma automática

---
+++ P4/video.js
@@ -0,0 +1,22 @@
+console.log("Ejecutando JS...");
+
+//----- Obtener elemento de video y configurarlo
+const video1 = document.getElementById("video01");
+const video2 = document.getElementById("video02");
+const video3 = document.getElementById("video03");
+const display = document.getElementById("display");
+
+//-- Tamaño de la pantalla de video
+video1.width = 200;
+video1.height = 100;
+video2.width = 200;
+video2.height = 100;
+video3.width = 200;
+video3.height = 100;
+display.width = 400;
+display.height = 200;
+
+
+const play1 = document.getElementById("boton1");
+const play2 = document.getElementById("boton2");
+const play3 = document.getElementById("boton3");
