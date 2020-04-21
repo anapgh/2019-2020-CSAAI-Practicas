8ba4c5f4ca749df914480cb44d5999fda1a9bec3
anapgh <ap.garcia.2016@alumnos.urjc.es>
2020-04-21 13:35:03 +0200

    Hecho lo basico

--- P5/filtro.js
+++ P5/filtro.js
@@ -25,71 +25,101 @@ const colores = document.getElementById('colores')
 //-- la imagen no se carga instantaneamete, sino que lleva tiempo.
 //-- Solo podemos acceder a ella una vez que este cargada totalmente.
 img.onload = function(){
-
   console.log("Imagen cargada");
-
   //-- Se establece como tamaño del canvas el mismo que la imagen Original
   canvas.width = img.width;
   canvas.height = img.height;
-
   //-- Situar la imagen original en el canvas.
   //-- No se han hecho manipulaciones aun.
   ctx.drawImage(img, 0,0);
-
   console.log("Imagen lista...");
 };
 
-//-- Funcion de retrollamada al boton de colores
-colores.onclick = () => {
-  //-- Funcion de retrollamada del deslizador
+function filtroColores(data){
+  var umbralR = des_rojo.value;
+  var umbralG = des_verde.value;
+  var umbralB = des_azul.value;
+  //-- Filtrar la imagen según el nuevo umbral
+  for (var i = 0; i < data.length; i+=4) {
+      if (data[i] > umbralR){
+        data[i] = umbralR;
+      }
+      if (data[i+1] > umbralG){
+        data[i+1] = umbralG;
+      }
+      if (data[i+2] > umbralB){
+        data[i+2] = umbralB;
+      }
+    }
+}
+
+function deslizadores(){
+  ctx.drawImage(img, 0,0);
+  //-- Funcion de retrollamada de los deslizadores
   des_rojo.oninput = () => {
     //-- Mostrar el nuevo valor del deslizador
     value_rojo.innerHTML = des_rojo.value;
-
     //-- Situar la imagen original en el canvas
-    //-- No se han hecho manipulaciones todavia
     ctx.drawImage(img, 0,0);
-
     //-- Obtener la imagen del canvas en pixeles
     let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
     //-- Obtener el array con todos los píxeles
     let data = imgData.data
-
     //-- Obtener el umbral de la COMPONENTE ROJA del deslizador
-    umbral = des_rojo.value
-
-    //-- Filtrar la imagen según el nuevo umbral
-    //-- Cambiandole el valor de la componente roja
-    for (let i = 0; i < data.length; i+=4) {
-      if (data[i] > umbral) //-- Si es mayor que el umbralm le asignamos el valor umbral
-        data[i] = umbral;
-    }
-
+    filtroColores(data);
     //-- Poner la imagen modificada en el canvas
+    ctx.putImageData(imgData, 0, 0);
+  }
+
+  des_verde.oninput = () => {
+    value_verde.innerHTML = des_verde.value;
+    ctx.drawImage(img, 0,0);
+    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let data = imgData.data
+    //-- Obtener el umbral de la COMPONENTE VERDE del deslizador
+    filtroColores(data);
+    ctx.putImageData(imgData, 0, 0);
+  }
+
+  des_azul.oninput = () => {
+    value_azul.innerHTML = des_azul.value;
+    ctx.drawImage(img, 0,0);
+    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let data = imgData.data
+    //-- Obtener el umbral de la COMPONENTE AZUL del deslizador
+    filtroColores(data);
     ctx.putImageData(imgData, 0, 0);
   }
 }
 
-
-
-//-- Función de retrollamada al boton de GRISES
-grises.onclick = () => {
-  var brillo = 0;
-  //-- Obtenemos la imagen del canvas en pixeles
+function filtroGrises(){
+  ctx.drawImage(img, 0,0);
   let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
-  //-- Array de todos los pixeles
-  let data = imgData.data; // Cada 4 posiciones corresponden a un punto de pixel
-
+  let data = imgData.data;
   //-- Calcular el brillo para CADA PIXEL y ponerselo por igual a cada componente
   for (var i = 0; i < data.length; i+=4) {
-    brillo = (3 * data[i] + 4 * data[i+1] + data[i+2])/8
+    r = data[i];
+    g = data[i+1];
+    b = data[i+2];
+    brillo = (3 * r + 4 * g + b)/8
     data[i] = brillo;
     data[i+1] = brillo;
     data[i+2] = brillo;
   }
-
-  //-- Actualizamos  la imagen del canvas con los nuevos datos
   ctx.putImageData(imgData, 0, 0);
+}
+
+//-- Función de retrollamada al boton de GRISES
+grises.onclick = () => {
+  console.log('Boton grises');
+  filtroGrises();
+}
+
+//-- Funcion de retrollamada al boton COLORES
+colores.onclick = () => {
+  console.log('Boton colores');
+  des_rojo.value = 255;
+  des_verde.value = 255;
+  des_azul.value = 255;
+  deslizadores();
 }
